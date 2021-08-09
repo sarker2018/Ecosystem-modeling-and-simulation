38052e14074f80843e08cdeb9f30af2851d84ec7
sarker2018 <61450446+sarker2018@users.noreply.github.com>
2021-08-09 13:40:59 +0200

    Delete scripts directory

--- scripts/light_limitation.R
+++
@@ -1,4 +0,0 @@
-ramp_function = function(Max_Synth_pm, intensity_I, Half_Satu_K){
-  val = Max_Synth_pm*(intensity_I/(2*Half_Satu_K))
-  val[intensity_I>=2*Half_Satu_K] = Max_Synth_pm
-  return(val)}
